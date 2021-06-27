503e574298bb8bc34ac97f01832485f4ae0beee1
anantha narayanank <ananthun111@gmail.com>
2021-06-27 16:54:52 +0530

    completed

--- js/main.js
+++ js/main.js
@@ -8,3 +8,18 @@
      target.style.transform ='translate3d(0px, '+rate+'px,0px)';
 
  })
+ ScrollReveal().reveal('.banner-container', {
+    delay: 300,
+    duration: 500,
+    distance: '60px',
+    origin:'top',
+    opacity: .2,
+    reset: true
+});
+ScrollReveal().reveal('.items,.item', {
+    delay: 300,
+    duration: 800,
+    distance: '10px',
+    opacity: .2,
+    reset: true
+});
